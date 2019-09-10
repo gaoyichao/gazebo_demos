f147dbf2d2899baac39a7e4cdfdccff947b71ec0
gaoyichao <gaoyichao2013@126.com>
2019-09-10 08:28:23 +0800

    velodyne 获取激光扫描数据

--- demos/velodyne/vel.cc
+++ demos/velodyne/vel.cc
@@ -4,6 +4,11 @@
 
 #include <gazebo/gazebo_client.hh>
 
+void cb(ConstLaserScanStampedPtr & _msg)
+{
+    std::cout << _msg->DebugString() << std::endl;
+}
+
 int main(int _argc, char **_argv)
 {
     std::cerr << "GAZEBO_MAJOR_VERSION:" << GAZEBO_MAJOR_VERSION << std::endl;
@@ -13,6 +18,7 @@ int main(int _argc, char **_argv)
     node->Init();
 
     gazebo::transport::PublisherPtr pub = node->Advertise<gazebo::msgs::Vector3d>("~/my_velodyne/vel_cmd");
+    gazebo::transport::SubscriberPtr sub = node->Subscribe("~/my_velodyne/velodyne_hdl-32/top/sensor/scan", cb);
 
     pub->WaitForConnection();
 
@@ -20,6 +26,9 @@ int main(int _argc, char **_argv)
     gazebo::msgs::Set(&msg, ignition::math::Vector3d(std::atof(_argv[1]), 0, 0));
   
     pub->Publish(msg);
+
+    for (int i = 0; i < 10; i++)
+        gazebo::common::Time::MSleep(10);
 
     gazebo::client::shutdown();
 }
