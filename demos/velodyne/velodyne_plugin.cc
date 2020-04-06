ffa4d50f568aeefe806761bad74e1b1146e2a817
gaoyichao <gaoyichao2013@126.com>
2020-04-06 08:49:28 +0800

    调整命名

--- demos/velodyne/velodyne_plugin.cc
+++ demos/velodyne/velodyne_plugin.cc
@@ -15,31 +15,26 @@
 
 namespace gazebo
 {
-    // 一个控制Velodyne传感器的插件，它是一个Model类型的插件，所以继承自ModelPlugin
     class VelodynePlugin : public ModelPlugin
     {
-        // 构造函数
         public: VelodynePlugin() {}
 
-        // 在插入本插件的时候，Gazebo就会调用该函数。
-        // 该函数是一个虚函数，我们可以通过重载该函数来对插件进行一些初始化的操作。
-        // 输入参数_model是一个指向与本插件相关联的模型的指针。
-        // 输入参数_sdf则是一个指向本插件SDF元素的指针。
-        public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+        /*
+         * Load - 在插入本插件的时候, Gazebo就会调用该函数, 进行一些初始化的操作.
+         * 
+         * @model: 指向与本插件相关联的模型的指针
+         * @sdf: 指向本插件SDF元素的指针
+         */
+        public: virtual void Load(physics::ModelPtr model, sdf::ElementPtr sdf)
         {
-           if (_model->GetJointCount() == 0) {
-               std::cerr << "没有正确装载velodyne模型\n";
-               return;
-           }
-           this->model = _model;
-           this->joint = _model->GetJoints()[0];
-           this->pid = common::PID(0.1, 0, 0);
-           this->model->GetJointController()->SetVelocityPID(this->joint->GetScopedName(), this->pid);
-
-            this->node = transport::NodePtr(new transport::Node());
-            this->node->Init(this->model->GetWorld()->Name());
-            std::string topicName = "~/" + this->model->GetName() + "/vel_cmd";
-            this->sub = this->node->Subscribe(topicName, &VelodynePlugin::OnMsg, this);
+            if (model->GetJointCount() == 0) {
+                std::cerr << "没有正确装载velodyne模型\n";
+                return;
+            }
+            this->mModel = model;
+            this->mJoint = model->GetJoints()[0];
+            this->mPid = common::PID(0.1, 0, 0);
+            this->mModel->GetJointController()->SetVelocityPID(this->mJoint->GetScopedName(), this->mPid);
 
             // ROS 配置
             if (!ros::isInitialized()) {
@@ -50,42 +45,44 @@ namespace gazebo
             this->mRosNode.reset(new ros::NodeHandle("gazebo_demos"));
 
             ros::SubscribeOptions so = ros::SubscribeOptions::create<std_msgs::Float32>(
-                    "/" + this->model->GetName() + "/vel_cmd",
+                    "/" + this->mModel->GetName() + "/vel_cmd",
                     1,
-                    boost::bind(&VelodynePlugin::OnRosMsg, this, _1),
+                    boost::bind(&VelodynePlugin::OnVelCmdMsgFromROS, this, _1),
                     ros::VoidPtr(), &this->mRosQueue);
             this->mRosSub = this->mRosNode->subscribe(so);
-            this->mRosQueueThread = std::thread(std::bind(&VelodynePlugin::QueueThread, this));
+            this->mRosQueueThread = std::thread(std::bind(&VelodynePlugin::RosQueueThread, this));
         }
 
-        // 设定Velodyne的转速
-        // 输入参数_vel定义了控制的目标转速
-        void SetVelocity(const double &_vel)
+        /*
+         * SetVelocity - 设定Velodyne的转速
+         * 
+         * @vel: 激光雷达的扫描转速, rad/s
+         */
+        public: void SetVelocity(const double &vel)
         {
-            this->model->GetJointController()->SetVelocityTarget(this->joint->GetScopedName(), _vel);
+            this->mModel->GetJointController()->SetVelocityTarget(this->mJoint->GetScopedName(), vel);
         }
 
-        // 处理接收的消息 
-        // 输入参数_msg记录了接收到的消息内容，是一个Vector3的消息，这里只用到其中的x分量。
-        private: void OnMsg(ConstVector3dPtr &_msg)
-        {
-            this->SetVelocity(_msg->x());
-            //std::cout << this->model->GetJoint("joint")->Position() * 180 / 3.1415926 << std::endl;
-            std::cout << "DOF: " << this->joint->GetChildCount() << std::endl;
-            //std::cout << this->joint->GetAngle(0) * 180 / 3.1415926 << std::endl;
-        }
-
-        private: void OnRosMsg(const std_msgs::Float32ConstPtr & msg)
+        /*
+         * OnVelCmdMsgFromROS - 接收到来自ROS系统的调速消息的回调函数
+         * 
+         * @msg: 调速消息
+         */
+        private: void OnVelCmdMsgFromROS(const std_msgs::Float32ConstPtr & msg)
         {
             this->SetVelocity(msg->data);
         }
 
-        private: void QueueThread()
+        /*
+         * RosQueueThread - 处理ROS消息队列的线程
+         */
+        private: void RosQueueThread()
         {
             static const double timeout = 0.01;
             while (this->mRosNode->ok())
                 this->mRosQueue.callAvailable(ros::WallDuration(timeout));
         }
+
 
         private:
             std::unique_ptr<ros::NodeHandle> mRosNode;
@@ -93,12 +90,12 @@ namespace gazebo
             ros::CallbackQueue mRosQueue;
             std::thread mRosQueueThread;
 
-            physics::ModelPtr model;
-            physics::JointPtr joint;
-            common::PID pid;
+            physics::ModelPtr mModel;
+            physics::JointPtr mJoint;
+            common::PID mPid;
 
-            transport::NodePtr node;
-            transport::SubscriberPtr sub;
+            gazebo::transport::NodePtr mGazeboNode;
+            gazebo::transport::SubscriberPtr mLaserScanSub;
     };
 
     // 向Gazebo注册本插件
