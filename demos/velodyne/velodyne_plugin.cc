5ab4cf5cba6ade012d126de94b133013043571b3
gaoyichao <gaoyichao2013@126.com>
2020-04-22 21:48:24 +0800

    输出激光点云数据

--- demos/velodyne/velodyne_plugin.cc
+++ demos/velodyne/velodyne_plugin.cc
@@ -10,9 +10,21 @@
 #include <gazebo/msgs/msgs.hh>
 #include <gazebo/transport/Node.hh>
 #include <gazebo/transport/transport.hh>
+#include <gazebo/msgs/laserscan_stamped.pb.h>
 #include <gazebo/msgs/msgs.hh>
 
+#include <ignition/math.hh>
+#include <ignition/msgs.hh>
 #include <ignition/math/Angle.hh>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/console/parse.h>
+
+#include <Eigen/Dense>
 
 #include <ros/ros.h>
 #include <ros/callback_queue.h>
@@ -26,6 +38,7 @@
 #include <geometry_msgs/TransformStamped.h>
 #include <std_msgs/Float32.h>
 #include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud2.h>
 
 #include <gazebo_demos/SetPIDParams.h>
 #include <SetPIDParamRequest.pb.h>
@@ -41,10 +54,14 @@ namespace gazebo
 
     class VelodynePlugin : public ModelPlugin
     {
-        public: VelodynePlugin() {}
-                ~VelodynePlugin() {
-                    delete mTfBr;
-                }
+        public: VelodynePlugin()
+        {
+            mPCCount = 0;
+            mPointCloud.clear();
+        }
+
+        public: ~VelodynePlugin() {
+        }
 
         /*
          * Load - 在插入本插件的时候, Gazebo就会调用该函数, 进行一些初始化的操作.
@@ -65,6 +82,7 @@ namespace gazebo
             this->mJoint = model->GetJoints()[0];
             this->mPid = common::PID(0.1, 0, 0);
             this->mModel->GetJointController()->SetVelocityPID(this->mJoint->GetScopedName(), this->mPid);
+            this->mModel->GetJointController()->SetVelocityTarget(this->mJoint->GetScopedName(), 20);
 
             // ROS 配置
             this->mRosNode.reset(new ros::NodeHandle("gazebo_demos"));
@@ -74,58 +92,20 @@ namespace gazebo
                     1,
                     boost::bind(&VelodynePlugin::OnVelCmdMsgFromROS, this, _1),
                     ros::VoidPtr(), &this->mRosQueue);
-            this->mRosSub = this->mRosNode->subscribe(so);
+            this->mRosVelSub = this->mRosNode->subscribe(so);
             ros::AdvertiseServiceOptions aso = ros::AdvertiseServiceOptions::create<gazebo_demos::SetPIDParams>(
                     "/" + this->mModel->GetName() + "/SetPIDParams",
                     boost::bind(&VelodynePlugin::OnSetPIDParamSrvFromROS, this, _1, _2),
                     ros::VoidPtr(), &this->mRosQueue);
             this->mRosSetPIDParamSrv = this->mRosNode->advertiseService(aso);
+            this->mRosPointCloudPub = this->mRosNode->advertise<sensor_msgs::PointCloud2>("/point_cloud_3d", 10);
 
             // Laser Scan
             this->mGazeboNode = gazebo::transport::NodePtr(new gazebo::transport::Node());
             this->mGazeboNode->Init();
             mLaserScanSub = this->mGazeboNode->Subscribe("~/my_velodyne/velodyne_hdl-32/top/sensor/scan", &VelodynePlugin::OnLaserScanMsg, this);
-            this->mRosPub = this->mRosNode->advertise<sensor_msgs::LaserScan>("/laserscan", 10);
-            this->mTfBr = new tf2_ros::TransformBroadcaster();
-
-            mSetPIDParamSub = this->mGazeboNode->Subscribe("~/my_velodyne/set_pid_param", &VelodynePlugin::OnSetPIDParamMsg, this);
-
-            mPlotFlag = false;
-            mSimTime = mWorld->SimTime();
-            mUpdateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&VelodynePlugin::OnWorldUpdateBegin, this)); 
 
             this->mRosQueueThread = std::thread(std::bind(&VelodynePlugin::RosQueueThread, this));
-        }
-
-
-        private:
-            event::ConnectionPtr mUpdateConnection;
-            common::Time mSimTime;
-            double mVelCmd;
-            std::vector<double> mPlotTimes;
-            std::vector<double> mPlotVels;
-            std::vector<double> mPlotCmds;
-            bool mPlotFlag;
-
-        private: void OnWorldUpdateBegin()
-        {
-            common::Time curTime = mWorld->SimTime();
-            mSimTime = curTime;
-            
-            if (mPlotFlag) {
-                if (mPlotTimes.size() > 100) {
-                    mPlotFlag = false;
-                }
-    
-                mPlotTimes.push_back(curTime.Double());
-                mPlotVels.push_back(this->mJoint->GetVelocity(0));
-                mPlotCmds.push_back(mVelCmd);
-                plt::clf();
-                plt::plot(mPlotTimes, mPlotVels);
-                plt::plot(mPlotTimes, mPlotCmds);
-                plt::xlim(mPlotTimes.front(), mPlotTimes.back());
-                plt::pause(0.001);
-            }
         }
 
         /*
@@ -135,11 +115,6 @@ namespace gazebo
          */
         public: void SetVelocity(const double &vel)
         {
-            mPlotFlag = true;
-            mPlotTimes.clear();
-            mPlotVels.clear();
-            mPlotCmds.clear();
-            mVelCmd = vel;
             this->mModel->GetJointController()->SetVelocityTarget(this->mJoint->GetScopedName(), vel);
         }
         /*
@@ -152,6 +127,12 @@ namespace gazebo
             this->SetVelocity(msg->data);
         }
 
+        /*
+         * OnSetPIDParamSrvFromROS - 接收来自ROS系统的PID调参服务请求的回调函数
+         * 
+         * @req: 请求数据
+         * @res: 响应结果
+         */
         private: bool OnSetPIDParamSrvFromROS(gazebo_demos::SetPIDParams::Request & req, gazebo_demos::SetPIDParams::Response & res)
         {
             mPid.SetPGain(req.P);
@@ -169,66 +150,58 @@ namespace gazebo
         {
             static const double timeout = 0.01;
             while (this->mRosNode->ok()) {
-                while (!this->mLaserScanQueue.empty()) {
-                    sensor_msgs::LaserScan &laser_msg = mLaserScanQueue.front();
-                    this->mRosPub.publish(laser_msg);
-                    mLaserScanQueue.pop_front();
-                }
                 this->mRosQueue.callAvailable(ros::WallDuration(timeout));
             }
         }
 
-        private: void OnSetPIDParamMsg(SetPIDParamRequestPtr & msg)
-        {
-            gazebo_demos_proto::msgs::SetPIDParamRequest request;
-            mPid.SetPGain(msg->pgain());
-            mPid.SetIGain(msg->igain());
-            mPid.SetDGain(msg->dgain());
-            this->mModel->GetJointController()->SetVelocityPID(this->mJoint->GetScopedName(), this->mPid);
-        }
-
+        /*
+         * OnLaserScanMsg - 接收到Gazebo中雷达扫描数据消息的回调函数
+         * 
+         * @msg: 雷达扫描数据
+         */
         private: void OnLaserScanMsg(ConstLaserScanStampedPtr & msg)
         {
-            sensor_msgs::LaserScan laser_msg;
-            laser_msg.header.stamp = ros::Time(msg->time().sec(), msg->time().nsec());
-            laser_msg.header.frame_id = "top";
-			laser_msg.angle_min = msg->scan().angle_min();
-  			laser_msg.angle_max = msg->scan().angle_max();
-  			laser_msg.angle_increment = msg->scan().angle_step();
-  			laser_msg.time_increment = 0;  // instantaneous simulator scan
-  			laser_msg.scan_time = 0;  // not sure whether this is correct
-  			laser_msg.range_min = msg->scan().range_min();
-  			laser_msg.range_max = msg->scan().range_max();
-  			laser_msg.ranges.resize(msg->scan().ranges_size());
-  			std::copy(msg->scan().ranges().begin(), msg->scan().ranges().end(), laser_msg.ranges.begin());
-  			laser_msg.intensities.resize(msg->scan().intensities_size());
-  			std::copy(msg->scan().intensities().begin(), msg->scan().intensities().end(), laser_msg.intensities.begin());
-            mLaserScanQueue.push_back(laser_msg);
+            gazebo::msgs::Pose const & pose_msg = msg->scan().world_pose();
+            ignition::math::Pose3d pose = gazebo::msgs::ConvertIgn(pose_msg);
+            Eigen::Quaterniond qua(pose.Rot().W(), pose.Rot().X(), pose.Rot().Y(), pose.Rot().Z());
 
-            gazebo::common::Time now = this->mModel->GetWorld()->SimTime();
-            double position = mJoint->Position();
-            double c = std::cos(position);
-            double s = std::sin(position);
-            tf2::Matrix3x3 rot(c,  0,  s,
-                               s,  0, -c,
-                               0,  1,  0);
-            tf2::Vector3 ori(0, 0, 0);
-            tf2::Transform trans(rot, ori);
-            geometry_msgs::TransformStamped stampedTrans;
-            stampedTrans.transform = tf2::toMsg(trans);
-            stampedTrans.header.frame_id = "base";
-            stampedTrans.header.stamp = ros::Time(now.sec, now.nsec);
-            stampedTrans.child_frame_id = "top";
-            mTfBr->sendTransform(stampedTrans);
+            int count = msg->scan().count();
+            double angle_min = msg->scan().angle_min();
+            double angle_step = msg->scan().angle_step();
+            double range_min = msg->scan().range_min();
+            double range_max = msg->scan().range_max();
+
+            for (int i = 0; i < count; i++) {
+                double range = msg->scan().ranges(i);
+                if (std::isinf(range) || range > range_max || range < range_min)
+                    continue;
+
+                double angle = angle_min + angle_step * i;
+				Eigen::Vector3d p(range * cos(angle), range * sin(angle), 0);
+                p = qua.matrix() * p;
+                pcl::PointXYZ point(p[0] + pose.Pos().X(), p[1] + pose.Pos().Y(), p[2] + pose.Pos().Z());
+        		mPointCloud.push_back(point);  
+            }
+            mPCCount++;
+
+            if (mPCCount > 100) {
+                sensor_msgs::PointCloud2 pc_msg;
+                pcl::toROSMsg(mPointCloud, pc_msg);
+                pc_msg.header.stamp = ros::Time(msg->time().sec(), msg->time().nsec());
+                pc_msg.header.frame_id = "base";
+                this->mRosPointCloudPub.publish(pc_msg);
+
+                mPCCount = 0;
+                mPointCloud.clear();
+            }
         }
 
         private:
             std::unique_ptr<ros::NodeHandle> mRosNode;
-            ros::Subscriber mRosSub;
-            ros::Publisher mRosPub;
+            ros::Subscriber mRosVelSub;
+            ros::Publisher mRosPointCloudPub;
             ros::ServiceServer mRosSetPIDParamSrv;
             ros::CallbackQueue mRosQueue;
-            tf2_ros::TransformBroadcaster *mTfBr;
             std::thread mRosQueueThread;
 
             physics::WorldPtr mWorld;
@@ -238,8 +211,10 @@ namespace gazebo
 
             gazebo::transport::NodePtr mGazeboNode;
             gazebo::transport::SubscriberPtr mLaserScanSub;
-            gazebo::transport::SubscriberPtr mSetPIDParamSub;
-            std::deque<sensor_msgs::LaserScan> mLaserScanQueue;
+
+            private: pcl::PointCloud<pcl::PointXYZ> mPointCloud;
+            private: int mPCCount;
+
     };
 
     // 向Gazebo注册本插件
