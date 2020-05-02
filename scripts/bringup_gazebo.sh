b840b8cdc6b807797e3c38c48c1e3d23a4ab20ef
gaoyichao <gaoyichao2013@126.com>
2020-05-02 19:54:34 +0800

    调整脚本，删除setup.sh

---
+++ scripts/bringup_gazebo.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+[ -L ${0} ] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR=$(dirname ${SCRIPT_DIR})
+WORLD_DIR=${SCRIPT_DIR}/../worlds 
+
+export GAZEBO_DEMO_PATH=$(dirname ${SCRIPT_DIR})
+export GAZEBO_MODEL_PATH=${GAZEBO_DEMO_PATH}/models:$GAZEBO_MODEL_PATH
+
+if [ -f ${1} ]
+then
+    gazebo ${1} -s `catkin_find --first-only libgazebo_demos_ros_plugin.so`
+fi
+
--- scripts/velodyne_world.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-[ -L ${0} ] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
-SCRIPT_DIR=$(dirname ${SCRIPT_DIR})
-WORLD_DIR=${SCRIPT_DIR}/../worlds 
-
-gazebo ${WORLD_DIR}/velodyne.world -s `catkin_find --first-only libgazebo_demos_ros_plugin.so`
-
