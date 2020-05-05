97a95e30fd9766a42d34d6439d6359f8caaf8526
gaoyichao <gaoyichao2013@126.com>
2020-05-05 22:04:16 +0800

    添加里程计解算,发布base->odom的坐标变换

--- demos/diffcart/diffcart_plugin.cc
+++ demos/diffcart/diffcart_plugin.cc
@@ -47,11 +47,14 @@ namespace gazebo
     class DiffCartPlugin : public ModelPlugin
     {
         public: DiffCartPlugin()
+            : mOdomTfBr(NULL)
         {
         }
 
         public: ~DiffCartPlugin()
         {
+            if (NULL != mOdomTfBr)
+                delete mOdomTfBr;
         }
 
         /*
@@ -78,9 +81,14 @@ namespace gazebo
             mLastCmdTime = mSimTime;
             mCmdTimeout = 0.1;
 
+            mXOdom = 0;
+            mYOdom = 0;
+            mAOdom = 0;
+
             // ROS 配置
             this->mRosNode.reset(new ros::NodeHandle("~"));
             mRosCmdVelSub = mRosNode->subscribe("/cmd_vel", 100, &DiffCartPlugin::OnCmdVelFromRos, this);
+            mOdomTfBr = new tf2_ros::TransformBroadcaster();
 
             mUpdateEndCnt = event::Events::ConnectWorldUpdateEnd(boost::bind(&DiffCartPlugin::OnWorldUpdateEnd, this));
         }
@@ -90,10 +98,12 @@ namespace gazebo
          */
         private: void OnWorldUpdateEnd()
         {
+            common::Time lastTime = mSimTime;
             mSimTime = mWorld->SimTime();
 
             ros::spinOnce();
 
+            UpdateOdometry(mSimTime - lastTime);
             AdjustVelocity();
         }
  
@@ -113,6 +123,9 @@ namespace gazebo
             mRightWheelCmd = msg->linear.x + msg->angular.z * mWheelDiff * 0.5;
         }
 
+        /*
+         * AdjustVelocity - 调整机器人运动速度
+         */
         private: void AdjustVelocity()
         {
             common::Time td = mSimTime - mLastCmdTime;
@@ -123,10 +136,51 @@ namespace gazebo
             mLeftWheel->SetVelocity(0, mLeftWheelCmd / mWheelRadius);
             mRightWheel->SetVelocity(0, mRightWheelCmd / mWheelRadius);
         }
+        /*
+         * UpdateOdometry - 更新里程计
+         * 
+         * @td: 更新时间间隔
+         */
+        private: void UpdateOdometry(common::Time td)
+        {
+            mLeftWheelVel = mLeftWheel->GetVelocity(0);
+            mRightWheelVel = mRightWheel->GetVelocity(0);
+
+            double ds_left = mWheelRadius * mLeftWheelVel * td.Double();
+            double ds_right = mWheelRadius * mRightWheelVel * td.Double();
+            if (std::isnan(ds_left))
+                ds_left = 0;
+            if (std::isnan(ds_right))
+                ds_right = 0;
+
+            double ds = 0.5 * (ds_left + ds_right);
+            double da = (ds_right - ds_left) / mWheelDiff;
+
+            mXOdom += ds * std::cos(mAOdom);
+            mYOdom += ds * std::sin(mAOdom);
+            mAOdom += da;
+
+            double c = std::cos(mAOdom);
+            double s = std::sin(mAOdom);
+            tf2::Vector3 ori(mXOdom, mYOdom, 0);
+            tf2::Matrix3x3 rot(c, -s, 0,
+                               s,  c, 0,
+                               0,  0, 1);
+            tf2::Transform trans(rot, ori);
+
+            geometry_msgs::TransformStamped odom;
+            odom.header.stamp = ros::Time(mSimTime.sec, mSimTime.nsec);
+            odom.header.frame_id = "odom";
+            odom.child_frame_id = "base";
+            odom.transform = tf2::toMsg(trans);
+            if (NULL != mOdomTfBr)
+                mOdomTfBr->sendTransform(odom);
+        }
 
         private:
             std::unique_ptr<ros::NodeHandle> mRosNode;
 			ros::Subscriber mRosCmdVelSub;
+            tf2_ros::TransformBroadcaster *mOdomTfBr;
 
             physics::WorldPtr mWorld;
             physics::ModelPtr mModel;
@@ -135,10 +189,18 @@ namespace gazebo
 
             event::ConnectionPtr mUpdateEndCnt;
 
-            double mWheelDiff;
-            double mWheelRadius;
-            double mLeftWheelCmd;
-            double mRightWheelCmd;
+            double mWheelDiff;      // 轮间距
+            double mWheelRadius;    // 轮半径
+
+            double mLeftWheelCmd;   // 左轮速度指令
+            double mLeftWheelVel;   // 左轮速度状态
+
+            double mRightWheelCmd;  // 右轮速度指令
+            double mRightWheelVel;  // 右轮速度状态
+
+            double mXOdom;
+            double mYOdom;
+            double mAOdom;
     };
 
     GZ_REGISTER_MODEL_PLUGIN(DiffCartPlugin)
